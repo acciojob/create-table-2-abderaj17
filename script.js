2614a9c251f9e3a1714da59cee4cb5b8fcf664ab
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-17 13:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function createTable() {
     //Write your code here
-  
+const rows = parseInt(prompt("Input number of rows"), 10);
+	if(isNaN(rows) || rows <= 0){
+		alert("Enter a valid positive number");
+		return;
+	} 
+
+	const columns = parseInt(prompt("Input number of coloumns"), 10);
+	if(isNaN(columns) || columns <= 0){
+		alert("Enter a valid positive number");
+		return;
+	}
+	
+	   const table = document.getElementById("myTable");
+
+table.innerHTML = "";
+
+for(let i = 0; i < rows; i++){
+	const row = table.insertRow();
+for(let j = 0; j < columns; j++){
+	const cell = row.insertCell(); 
+	table.textContent = `Row-${i} Column-${j}`;
+}	
+}
 }
